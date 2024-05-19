53a500e7be0f912817dcc7c21e6a6dc1384371dc
DhirajF <dhiraj.fegade101@gmail.com>
2024-05-19 17:58:46 +0530

    Develop batch file which directly compile all cpp files

--- Structural Design Patterns/Bridge Pattern/build.bat
+++ Structural Design Patterns/Bridge Pattern/build.bat
@@ -1,12 +1,35 @@
+@echo off
 cls
 
 del *.exe
 
-del *.res
-
 del *.obj
 
-cl.exe /c /EHsc MySQLDatabase.cpp PostgreSQLDatabase.cpp SQLiteDatabase.cpp Database.cpp  main.cpp 
+setlocal enabledelayedexpansion
 
-link.exe MySQLDatabase.obj PostgreSQLDatabase.obj SQLiteDatabase.obj Database.obj  main.obj user32.lib /SUBSYSTEM:CONSOLE /FeBridge.exe
+for /F "usebackq tokens=*" %%i in (`cd`) do (
+    set directory=%%i
+)
 
+echo %directory%
+
+rem Prompt the user for the name of the executable
+set /p exeName="Enter the name of the executable (without extension): "
+
+rem Check if the user entered a name
+if "%exeName%"=="" (
+    echo No name provided. Exiting.
+    exit /b 1
+)
+
+set "fileList="
+
+rem Loop through each file in the directory
+for %%f in ("%directory%\*.cpp") do (
+    rem Append the file name to the variable
+    set "fileList=!fileList! %%~nxf"
+)
+
+Cl /EHsc %fileList% /Fe%exeName%
+
+endlocal
